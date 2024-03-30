883fd82174e61bb8d4f958b0930c74bc24527dd6
juaneins_uio <ballesterosjg@gmail.com>
2024-03-30 11:42:33 -0500

    creacion components movements y resume, se agrega comp a layout

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,19 @@
 <script setup>
 import LayoutComponent from '@/components/LayoutComponent.vue';
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import MovementsComponent from '@/components/MovementsComponent.vue';
+import ResumeComponent from '@/components/Resume/ResumeComponent.vue';
 </script>
 <template>
   <LayoutComponent>
     <template #header>
       <HeaderComponent />
+    </template>
+    <template #resume>
+      <ResumeComponent />
+    </template>
+    <template #movements>
+      <MovementsComponent />
     </template>
   </LayoutComponent>
 </template>

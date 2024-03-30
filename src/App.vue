96c62a6df7d1ea43dfdd7c054e8d2195b6f62da5
juaneins_uio <ballesterosjg@gmail.com>
2024-03-29 21:05:43 -0500

    Creando slots default y fallback pantalla inicial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup>
-import HomeComponent from '@/components/HomeComponent.vue';
+import SplashScreenComponent from '@/components/SplashScreenComponent.vue';
+import { defineAsyncComponent } from 'vue';
+
+const HomeAsyncComponent = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('@/components/HomeComponent.vue'));
+    }, 2500);
+  });
+});
 </script>
 
 <template>
-  <HomeComponent />
+  <Suspense>
+    <template #default>
+      <HomeAsyncComponent />
+    </template>
+    <template #fallback>
+      <SplashScreenComponent />
+    </template>
+  </Suspense>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
